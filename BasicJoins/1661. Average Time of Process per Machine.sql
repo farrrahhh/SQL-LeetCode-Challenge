cb9370cf1647b2ddc23d5e652842818ce3251498
farrrahhh <18222096@std.stei.itb.ac.id>
2025-03-23 00:58:30 +0700

    577

--- BasicJoins/1661. Average Time of Process per Machine.sql
+++ BasicJoins/1661. Average Time of Process per Machine.sql
@@ -82,3 +82,17 @@ JOIN Activity a2
     AND a1.activity_type = 'start'
     AND a2.activity_type = 'end'
 GROUP BY a1.machine_id;
+
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a, 
+     Activity b
+WHERE 
+    a.machine_id = b.machine_id
+AND 
+    a.process_id = b.process_id
+AND 
+    a.activity_type = 'start'
+AND 
+    b.activity_type = 'end'
+GROUP BY machine_id
